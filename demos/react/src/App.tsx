57b131226445a0175db7ecbd3b12dc73823ad1f9
heyongqi10 <heyongqi10@jd.com>
2024-10-06 22:36:14 +0800

    chore(demo): format

--- demos/react/src/App.tsx
+++ demos/react/src/App.tsx
@@ -1,13 +1,17 @@
-import cls from "@xxhls/classnames"
+import cls from "@xxhls/classnames";
 
 function App() {
-  return (
-    <>
-      <h1 className={cls('font-color-red', {
-        'background-color-blue': true
-      })}>Hello World</h1>
-    </>
-  )
+	return (
+		<>
+			<h1
+				className={cls("font-color-red", {
+					"background-color-blue": true,
+				})}
+			>
+				Hello World
+			</h1>
+		</>
+	);
 }
 
-export default App
+export default App;
--- demos/react/src/main.tsx
+++ demos/react/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './test.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./test.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root");
+if (root) {
+	const rootElement = createRoot(root);
+	rootElement.render(
+		<StrictMode>
+			<App />
+		</StrictMode>,
+	);
+}
